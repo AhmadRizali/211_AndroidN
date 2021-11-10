72871b1edd4657791dc79037c487e8d5e12ad1b9
AhmadRizali <19710064ahmadrizali@gmail.com>
2021-11-10 09:18:11 +0800

    Penggunaan Kotlin

--- app/src/main/java/com/AhmadRizali/a211androidn/MainActivity.kt
+++ app/src/main/java/com/AhmadRizali/a211androidn/MainActivity.kt
@@ -2,10 +2,30 @@ package com.AhmadRizali.a211androidn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class  MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val simpanButton = findViewById<Button>(R.id.simpanButton)
+        val batalButton = findViewById<Button>(R.id.batalButton)
+
+        val namaEditText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val numberEditText = findViewById<EditText>(R.id.editTextNumber)
+
+        simpanButton.setOnClickListener {
+            val nama = namaEditText.text.toString()
+            Toast.makeText( this, "Hai $nama", Toast.LENGTH_SHORT).show()
+        }
+        batalButton.setOnClickListener {
+            var angka = numberEditText.text.toString().trim().toInt()
+            angka ++
+            numberEditText.setText(angka.toString())
+        }
     }
 }
